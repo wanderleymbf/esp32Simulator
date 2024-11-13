6f3cbfd7790d04eb59614c5265d0c1bd7815c73a
wanderleymbf <wanderley.macedo@sabaojua.com.br>
2024-11-13 09:18:48 -0300

    feat/changing readme.md

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# esp32Simulator
+# Simulador ESP32 GUI e CLI
+
+Este projeto é uma simulação de um dispositivo ESP32, desenvolvido em Python com uma interface gráfica (GUI) usando `Tkinter` e um script de linha de comando (CLI) para enviar dados para uma API.
+
+## Funcionalidades
+
+- **GUI**: Interface gráfica para iniciar/parar a contagem, atualizar a URL da API e o campo `CODESPUNI`.
+- **CLI**: Script de linha de comando para definir o status (`on` ou `off`) e enviar diretamente para a API.
+
+## Requisitos
+
+Certifique-se de ter o Python 3.x instalado. Em seguida, instale as bibliotecas necessárias:
+
+```bash
+pip install requests
+```
+## Estrutura dos Scripts
+
+- **GUI**: Interface gráfica para interagir com o dispositivo simulador.
+- **CLI**: Script de linha de comando para definir o status diretamente na API.
+
+## Como Usar
+
+### Executando a GUI
+
+- No terminal, navegue até o diretório do projeto.
+
+- Execute o seguinte comando para iniciar a GUI:
+
+```bash
+python3 simulador_gui.py
+```
+
+### Executando a CLI
+
+Você também pode executar o simulador pela linha de comando para definir o status diretamente.
+
+- **1**: Execute o script simulador_cli.py com o seguinte comando:
+
+```bash
+python3 simulador_cli.py --status on
+```
+
+- **2**: Para definir o status como off, execute:
+
+```bash
+python3 simulador_cli.py --status off
+```
+
+
+
+
+
+
+
+
+
+
--- simulator_cli.py
+++ simulator_cli.py
@@ -3,7 +3,7 @@ import time
 import json
 import argparse
 
-# Função para processar os argumentos de linha de comando
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Simula o envio de dados do ESP32")
     parser.add_argument(
@@ -14,20 +14,20 @@ def parse_arguments():
     )
     return parser.parse_args()
 
-# Função principal
+
 def main():
-    # Obtém os argumentos da linha de comando
+
     args = parse_arguments()
-    status = args.status  # O estado inicial do dispositivo será passado aqui
+    status = args.status  
 
     url = "http://127.0.0.1:8000/api/store"
     
     while True:
-        # Obtém o timestamp atual
+
         counted_at = int(time.time())
 
         if status == "on":
-            # Dados do sensor quando em operação
+
             sensor_data = {
                 "sensor_data": {
                     "CODESP": "123",
@@ -36,7 +36,7 @@ def main():
                 }
             }
             
-            # Dados do dispositivo com status "on"
+
             device_status = {
                 "device_status": {
                     "NOME": "esp2",
@@ -48,16 +48,16 @@ def main():
                 }
             }
 
-            # Envia ambas as solicitações POST para simular o dispositivo funcionando
+
             response = requests.post(url, json=sensor_data)
             response2 = requests.post(url, json=device_status)
             
-            # Exibe a resposta da API
+
             print(f"Status Code: {response.status_code}, Response: {response.text}")
             print(f"Status Code: {response2.status_code}, Response: {response2.text}")
         
         else:
-            # Configura o status para "off" e envia apenas device_status
+
             device_status = {
                 "device_status": {
                     "NOME": "esp2",
@@ -69,11 +69,11 @@ def main():
                 }
             }
 
-            # Envia apenas o status do dispositivo como "off"
+
             response2 = requests.post(url, json=device_status)
             print(f"Status Code: {response2.status_code}, Response: {response2.text}")
         
-        # Aguarda 5 segundos antes do próximo envio
+
         time.sleep(5)
 
 # Executa o script principal
